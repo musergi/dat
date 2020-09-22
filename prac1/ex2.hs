12899c8c29f02eb1436fd254aad897e5b88431b4
Compte ldatusr33 <ldatusr33@soft00.labs-entel.local>
2020-09-22 19:57:25 +0200

    Fixed drawing of ex2 and run

--- prac1/ex2.hs
+++ prac1/ex2.hs
@@ -4,9 +4,10 @@ lightBulb :: Color -> Double -> Drawing
 lightBulb c y = colored c $ translated 0 y $ solidCircle 1
 
 myDrawing :: Drawing
-myDrawing = lightBulb red 1.5 <>
+myDrawing = rectangle 3 8 <>
+            lightBulb red 2.5 <>
             lightBulb yellow 0 <>
-            lightBulb green (-1.5)
+            lightBulb green (-2.5)
 
 main :: IO ()
 main = svgOf (myDrawing <> coordinatePlane)
