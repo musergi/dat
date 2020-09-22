093d803185ff2f6bc9de662c1a23c1c671359bdd
musergi <sergi.mus@gmail.com>
2020-09-22 20:03:47 +0200

    Superposed lights

--- prac1/ex2.hs
+++ prac1/ex2.hs
@@ -4,10 +4,10 @@ lightBulb :: Color -> Double -> Drawing
 lightBulb c y = colored c $ translated 0 y $ solidCircle 1
 
 myDrawing :: Drawing
-myDrawing = solidRectangle 3 8 <>
-            lightBulb red 2.5 <>
+myDrawing = lightBulb red 2.5 <>
             lightBulb yellow 0 <>
-            lightBulb green (-2.5)
+            lightBulb green (-2.5) <>
+            solidRectangle 3 8
 
 main :: IO ()
 main = svgOf (myDrawing <> coordinatePlane)
