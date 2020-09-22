4ea26d8e1fcdbb6161ce5b64cfe5e81e335a50a8
musergi <sergi.mus@gmail.com>
2020-09-22 20:01:22 +0200

    Made rect solid

--- prac1/ex2.hs
+++ prac1/ex2.hs
@@ -4,7 +4,7 @@ lightBulb :: Color -> Double -> Drawing
 lightBulb c y = colored c $ translated 0 y $ solidCircle 1
 
 myDrawing :: Drawing
-myDrawing = rectangle 3 8 <>
+myDrawing = solidRectangle 3 8 <>
             lightBulb red 2.5 <>
             lightBulb yellow 0 <>
             lightBulb green (-2.5)
