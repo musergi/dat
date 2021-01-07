d576fa2d928f12851e8dec10b038036ef3450824
Sergi Mus <sergi.mus@gmail.com>
2021-01-07 19:27:49 +0100

    Added new route and fixed indentation

--- prac4/forums-spa/src/app.js
+++ prac4/forums-spa/src/app.js
@@ -8,13 +8,13 @@ angular.module('forumsApp').constant('config',
     {
       // Settings for the OAuth2 Authorization Server
       serverSettings : {
-	authorizationEndpoint: 'http://soft0.upc.edu/~WEBprofe/oauth2/as.cgi/oauth2/authorization'
+        authorizationEndpoint: 'http://soft0.upc.edu/~WEBprofe/oauth2/as.cgi/oauth2/authorization'
       },
       // Settings for the OAuth2 registered client.
       // Please, change to your settings !
       clientSettings : {
-	client_id: '0d07c82c-0c80-4313-b72f-5d98e46583b5',
-	callback_url: 'http://soft0.upc.edu/dat/practica4/tutorial/src/step_5/oauth2/callback.html'
+        client_id: '0d07c82c-0c80-4313-b72f-5d98e46583b5',
+        callback_url: 'http://soft0.upc.edu/dat/practica4/tutorial/src/step_5/oauth2/callback.html'
       },
       // Settings for the Forums web service
       forumsApiUrl: 'http://soft0.upc.edu/~WEBprofe/oauth2/forums-service.cgi/api',
@@ -29,6 +29,9 @@ angular.module('forumsApp').config(['$routeProvider', function($routeProvider) {
         }).
         when('/forum-:forumId', {
             template: '<forum></forum>'
+        }).
+        when('/topic-:topicId', {
+            tempate: '<topic></topic>'
         }).
         otherwise('/');
 }]);
